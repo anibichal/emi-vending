2f88b578dff73568a11fe97030156607277ddca4
anibichal <anibalalfarobadilla@gmail.com>
2025-10-31 23:46:26 -0300

    ajuste en configuracion vite para probar en raspberry

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,       // 👈 aquí defines el puerto
+    host: true,       // opcional: permite acceder desde otras IPs (útil en Raspberry o kioskos)
+  },
 })
