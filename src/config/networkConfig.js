46b97ac25e21dac9f69cda8942f931141f4bc140
anibichal <anibalalfarobadilla@gmail.com>
2025-12-12 23:47:32 -0300

    agrega ws sale with port

--- src/config/networkConfig.js
+++ src/config/networkConfig.js
@@ -2,5 +2,5 @@ export const networkConfig = {
   wsUrlBidonStatus: 'ws://localhost:6789', 
   wsUrlStockLevel: 'ws://localhost:6780', 
   wsUrlServicioLlenado: 'ws://localhost:6781',
-  wsUrlServicioVenta: 'ws://localhost:6782',
+  wsUrlServicioVenta: 'ws://192.168.1.24:8080',
 }
