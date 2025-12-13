05740d1012d44531117804a036953de2bdec6ceb
anibichal <anibalalfarobadilla@gmail.com>
2025-12-13 00:36:26 -0300

    corrige ws name

--- src/config/networkConfig.js
+++ src/config/networkConfig.js
@@ -2,5 +2,5 @@ export const networkConfig = {
   wsUrlBidonStatus: 'ws://localhost:6789', 
   wsUrlStockLevel: 'ws://localhost:6780', 
   wsUrlServicioLlenado: 'ws://localhost:6781',
-  wsUrlServicioVenta: 'ws://localhost:6782',
+  wsUrlPosAutomatico: 'ws://localhost:6782',
 }
